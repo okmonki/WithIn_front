07b2b84ad8e5f88b1e95c7de49a972dcc9e340e9
Eun-sun-Lee <eunsun2080@kookmin.ac.kr>
2022-07-24 17:06:38 +0900

    [refactor] correct InterestActivity.kt

--- app/src/main/java/com/example/within_front/interest/InterestActivity.kt
+++ app/src/main/java/com/example/within_front/interest/InterestActivity.kt
@@ -9,22 +9,22 @@ import androidx.appcompat.app.AppCompatActivity
 import com.example.within_front.R
 
 class InterestActivity : AppCompatActivity() {
+    val interestMBTISpinner: Spinner by lazy{
+        findViewById(R.id.interestSpinner2)}
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_interest) //view 연결
 
-        val interestMBTISpinner: Spinner by lazy{
-            findViewById(R.id.interestSpinner2)}
-
         val interestMBTIList= listOf("INFJ","INFP","ENFJ","ENFP","ISTJ","ISFJ","ESTJ","ESFJ","ISTP","ISFP","ESTP","ESFP")
-        //val interestMBTIAdapter = ArrayAdapter<String>(this,R.layout.activity_interest, interestMBTIList)
-       // interestMBTISpinner.adapter=interestMBTIAdapter
-        //interestMBTISpinner.onItemSelectedListener=object: AdapterView.OnItemSelectedListener{
-          //  override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
-            //    interestMBT.text=
-
-           // }
-       // }
-
+        var interestMBTIAdapter = ArrayAdapter<String>(this,android.R.layout.simple_spinner_dropdown_item, interestMBTIList)
+        interestMBTISpinner.adapter=interestMBTIAdapter
+        interestMBTISpinner.onItemSelectedListener=object: AdapterView.OnItemSelectedListener{
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+            }
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+            }
         }
+
+    }
 }
