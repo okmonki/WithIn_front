53228da2f96cc0b36f32b789d64489825058e06e
Eun-sun-Lee <eunsun2080@kookmin.ac.kr>
2022-07-25 16:25:50 +0900

    [feat] loginActivity.kt : add login logic that check about email and password validation

--- app/src/main/java/com/example/within_front/MainActivity.kt
+++ app/src/main/java/com/example/within_front/MainActivity.kt
@@ -4,12 +4,13 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.within_front.interest.InterestActivity
+import com.example.within_front.login.LoginActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val intent= Intent(this, InterestActivity::class.java)
+        val intent= Intent(this, LoginActivity::class.java)
         startActivity(intent)
 
     }
--- app/src/main/java/com/example/within_front/login/LoginActivity.kt
+++ app/src/main/java/com/example/within_front/login/LoginActivity.kt
@@ -3,71 +3,57 @@ package com.example.within_front.login
 import android.content.Intent
 import android.media.Image
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.text.method.HideReturnsTransformationMethod
 import android.text.method.PasswordTransformationMethod
 import android.util.Log
 import android.view.View
-import android.widget.EditText
-import android.widget.ImageButton
-import android.widget.ImageView
-import android.widget.Spinner
+import android.widget.*
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.AppCompatButton
+import androidx.core.widget.addTextChangedListener
 import com.example.within_front.R
 import java.util.regex.Matcher
 import java.util.regex.Pattern
 
 class LoginActivity : AppCompatActivity() {
 
-    val logoImageView : ImageView by lazy{
+    val logoImageView: ImageView by lazy {
         findViewById(R.id.logoImageView)
     }
-    val emailEditText : EditText by lazy{
+    val emailEditText: EditText by lazy {
         findViewById(R.id.loginEmailEditText)
     }
-    val passwordEditText : EditText by lazy{
+    val passwordEditText: EditText by lazy {
         findViewById(R.id.loginPasswordEditText)
     }
-    val eyeImageButton : ImageButton by lazy{
+    val eyeImageButton: ImageButton by lazy {
         findViewById(R.id.check_password_1)
     }
 
-    val loginButton : AppCompatButton by lazy{
+    val loginButton: AppCompatButton by lazy {
         findViewById(R.id.loginButton)
     }
 
-    val signUpButton : AppCompatButton by lazy{
+    val signUpButton: AppCompatButton by lazy {
         findViewById(R.id.SignUpButton)
     }
 
     private var isPasswordShowing = false
+    val emailValidation = "^[_A-Za-z0-9-]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
+        initEmailAndPasswordEnable()
+        checkEmailTextWatcher()
+        initLoginButton()
+        //initSignupButton()
 
 
-        fun isEmail(email: String?): Boolean {
-            var returnValue = false
-            val regex = "^[_a-zA-Z0-9-\\.]+@[\\.a-zA-Z0-9-]+\\.[a-zA-Z]+$"
-            val p: Pattern = Pattern.compile(regex)
-            val m: Matcher = p.matcher(email)
-            if (m.matches()) {
-                returnValue = true
-            }
-            return returnValue
-        }
-        //  Log.d("LoginActivity", "returnvalue$")
-
-        fun checkEmail(email: String): Boolean = email.contains("@")
-        // given
-        val email = "test@test.com"
-        // when
-        val actual = checkEmail(email)
-        // then
-        assertTrue(actual)
     }
 
     private fun getInputEmail() : String {
@@ -78,20 +64,77 @@ class LoginActivity : AppCompatActivity() {
         return passwordEditText.text.toString()
     }
 
-
-    private fun initSignupButton() {
-        //아직 SignUp페이지랑 merge 안해서 빨간 글씨
-        signUpButton.setOnClickListener {
-            val intent = Intent(this, SignUp.SignupActivity::class.java)
-            startActivity(intent)
+    private fun checkEmail():Boolean{
+        val email = getInputEmail().trim() //공백제거
+        val p = Pattern.matches(emailValidation, email) // 서로 패턴이 맞는지 확인
+        return if (p) {
+            //이메일 형태가 정상일 경우
+            emailEditText.setTextColor(R.color.black.toInt())
+            true
+        } else {
+            emailEditText.setTextColor(-65536)
+            //또는 questionEmail.setTextColor(R.color.red.toInt())
+            false
         }
     }
+
+    private fun checkEmailTextWatcher(){
+        emailEditText.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+                // text가 변경된 후 호출
+                // s에는 변경 후의 문자열이 담겨 있다.
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                // text가 변경되기 전 호출
+                // s에는 변경 전 문자열이 담겨 있다.
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                // text가 바뀔 때마다 호출된다.
+                // 우린 이 함수를 사용한다.
+                checkEmail()
+            }
+        })
+
+        loginButton.setOnClickListener {
+            if(!checkEmail()){ //틀린 경우
+                Toast.makeText(applicationContext,"이메일 형식에 맞게 입력하세요!",Toast.LENGTH_LONG).show()
+            }
+            else{ //맞는 경우
+                Toast.makeText(applicationContext,"로그인 됐습니다.",Toast.LENGTH_LONG).show()
+                initLoginButton()
+            }
+        }
+    }
+
+//    private fun initSignupButton() {
+//        아직 SignUp 페이지랑 merge 안해서 빨간색 글씨
+//        signUpButton.setOnClickListener {
+//            val intent = Intent(this, SignUp.SignupActivity::class.java)
+//            startActivity(intent)
+//        }
+//    }
+
 
     private fun initLoginButton() {
         //login 버튼이 눌린다면
         loginButton.setOnClickListener {
             val email=getInputEmail()
             val password=getInputPassword()
+        }
+    }
+
+    private fun initEmailAndPasswordEnable() {
+        emailEditText.addTextChangedListener {
+            val enable=emailEditText.text.isNotEmpty() && passwordEditText.text.isNotEmpty()
+            loginButton.isEnabled=enable
+            signUpButton.isEnabled=enable
+        }
+        passwordEditText.addTextChangedListener {
+            val enable=emailEditText.text.isNotEmpty() && passwordEditText.text.isNotEmpty()
+            loginButton.isEnabled=enable
+            signUpButton.isEnabled=enable
         }
     }
 
@@ -111,18 +154,22 @@ class LoginActivity : AppCompatActivity() {
         }
     }
 
-    private fun initAlertDialog() { // 로그인 실패 시 alertDialog 띄워주는 함수
-        val dialogView = View.inflate(this, R.layout.alertdialog_view, null)
-        val builder = AlertDialog.Builder(this)
-        builder.setView(dialogView)
-        val dialog = builder.create()
-        dialog.window?.setBackgroundDrawableResource(android.R.color.transparent)
-        val dialogOkButton: AppCompatButton = dialogView.findViewById(R.id.okButton)
-        dialogOkButton.setOnClickListener {
-            dialog.dismiss()
-        }
-        dialog.show()
-    }
+//    private fun initAlertDialog() { // 로그인 실패 시 alertDialog 띄워주는 함수
+//        val dialogView = View.inflate(this, R.layout.alertdialog_view, null)
+//        val builder = AlertDialog.Builder(this)
+//        builder.setView(dialogView)
+//        val dialog = builder.create()
+//        dialog.window?.setBackgroundDrawableResource(android.R.color.transparent)
+//        val dialogOkButton: AppCompatButton = dialogView.findViewById(R.id.okButton)
+//        dialogOkButton.setOnClickListener {
+//            dialog.dismiss()
+//        }
+//        dialog.show()
+//    }
+
+
 
 
 }
+
+
