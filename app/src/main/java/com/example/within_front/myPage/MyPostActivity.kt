8368365217e465665dc4dc98dc80c49e13d2b9b2
YoonSangwon <yoonsw0532@naver.com>
2022-07-26 21:58:41 +0900

    feat : 내가 쓴 글 화면 RecyclerView adapter 구현

---
+++ app/src/main/java/com/example/within_front/myPage/MyPostActivity.kt
@@ -0,0 +1,30 @@
+package com.example.within_front.myPage
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.within_front.R
+
+class MyPostActivity : AppCompatActivity() {
+
+    private val myPostContainer : RecyclerView by lazy{
+        findViewById(R.id.my_post_container)
+    }
+
+    private val myPostList = mutableListOf<Post>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_mypost)
+
+        initRecyclerView()
+    }
+
+    private fun initRecyclerView(){
+        myPostContainer.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+        myPostContainer.setHasFixedSize(true)
+
+        myPostContainer.adapter = MyPostAdapter(this, myPostList)
+    }
+}
