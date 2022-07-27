edfe793c1399dd88a21170eb50b28f1015ea246d
SuperH0ng <ghdtmdgus100@gmail.com>
2022-07-27 23:07:10 +0900

    로그인 회원가입 / 유효성 검사 로직 email nickname password #22

--- app/src/main/java/com/example/within_front/MainActivity.kt
+++ app/src/main/java/com/example/within_front/MainActivity.kt
@@ -3,9 +3,9 @@ package com.example.within_front
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-    }
-}
+//class MainActivity  {
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_main)
+//    }
+//}
