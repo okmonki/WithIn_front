fb5b3dab762605f2f8bc891692eb6c467309c092
Eun-sun-Lee <eunsun2080@kookmin.ac.kr>
2022-07-29 14:39:43 +0900

    [style] change emailEditText color : blue -> mint

--- app/src/main/java/com/example/within_front/MainActivity.kt
+++ app/src/main/java/com/example/within_front/MainActivity.kt
@@ -10,7 +10,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val intent= Intent(this, InterestActivity::class.java)
+        val intent= Intent(this, LoginActivity::class.java)
         startActivity(intent)
 
     }
