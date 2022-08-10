ddef330b29b515ab0ae1832a8f76d1a7237a91bc
SuperH0ng <ghdtmdgus100@gmail.com>
2022-08-10 23:25:06 +0900

    [feat] 게시판 UI

--- app/src/main/java/com/example/within_front/MainActivity.kt
+++ app/src/main/java/com/example/within_front/MainActivity.kt
@@ -9,6 +9,6 @@ import com.example.within_front.login.LoginActivity
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_board_content)
     }
 }
