c12ef74e2b4cc0506d1413d9fe389e2cdd7fb058
SangwonYoon <38793142+SangwonYoon@users.noreply.github.com>
2022-08-12 15:20:15 +0900

    feat: 서버에서 내가 쓴 글 조회하는 기능 추가

--- app/src/main/java/com/example/within_front/myPage/MyPostActivity.kt
+++ app/src/main/java/com/example/within_front/myPage/MyPostActivity.kt
@@ -1,23 +1,31 @@
 package com.example.within_front.myPage
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.within_front.R
+import okhttp3.*
+import org.json.JSONArray
+import org.json.JSONObject
+import java.io.IOException
 
 class MyPostActivity : AppCompatActivity() {
+
+    val client = OkHttpClient()
 
     private val myPostContainer : RecyclerView by lazy{
         findViewById(R.id.my_post_container)
     }
 
-    private val myPostList = mutableListOf<Post>()
+    private var myPostList = mutableListOf<Post>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_mypost)
-        initRecyclerView()
+        // TODO getMyPost(userId)
     }
 
     private fun initRecyclerView(){
@@ -25,5 +33,46 @@ class MyPostActivity : AppCompatActivity() {
         myPostContainer.setHasFixedSize(true)
 
         myPostContainer.adapter = MyPostAdapter(this, myPostList)
+    }
+
+    private fun getMyPost(userId : Long){
+        val getMyPostRequest = Request.Builder().addHeader("Content-Type", "application/json").url("http:localhost:8080/user/$userId/posts").build()
+
+        client.newCall(getMyPostRequest).enqueue(object: Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                Log.d("connection error", "인터넷 연결 불안정")
+                runOnUiThread{
+                    Toast.makeText(
+                        this@MyPostActivity,
+                        "내가 쓴 글 조회에 실패했습니다. 다시 시도해주세요.",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                if(response.code() == 200){
+                    myPostList = mutableListOf()
+
+                    val jsonArray = JSONArray(response.body()!!.string())
+
+                    for(idx in 0 until jsonArray.length()){
+                        val tempPost = jsonArray[idx] as JSONObject
+                        val author = tempPost.getString("authorNickname")
+                        val content = tempPost.getString("content")
+                        val createdAt = tempPost.getString("createdAt")
+                        val board = tempPost.getString("boeadName")
+
+                        val post = Post(nickname = author, boardName = board, content = content, date = createdAt)
+                        myPostList.add(post)
+                    }
+                    myPostList.reverse()
+                    Log.d("myPostList", myPostList.size.toString())
+                    runOnUiThread{
+                        initRecyclerView()
+                    }
+                }
+            }
+        })
     }
 }
