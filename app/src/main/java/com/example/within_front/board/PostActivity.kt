5290adbfa6aeec7fcf8e540850d5dfd3b1519734
SuperH0ng <ghdtmdgus100@gmail.com>
2022-08-13 10:48:40 +0900

    [feat] recyclerView

--- app/src/main/java/com/example/within_front/board/PostActivity.kt
+++ app/src/main/java/com/example/within_front/board/PostActivity.kt
@@ -1,4 +1,33 @@
 package com.example.within_front.board
 
-class PostActivity {
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.within_front.R
+//import okhttp3.*
+import org.json.JSONArray
+import org.json.JSONObject
+import java.io.IOException
+
+class PostActivity : AppCompatActivity() {
+
+//    private val client = OkHttpClient()
+
+    private val commentContainer : RecyclerView by lazy{
+        findViewById(R.id.comments)
+    }
+
+    private var commentList = mutableListOf<Comment>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_post)
+        initRecyclerView()
+    }
+    private fun initRecyclerView(){
+        commentContainer.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+        commentContainer.setHasFixedSize(true)
+        commentContainer.adapter = CommentAdapter(this, commentList)
+    }
 }
