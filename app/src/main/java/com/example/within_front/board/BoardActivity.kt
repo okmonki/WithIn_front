40ff05223c08c32cf1adcc5777aaca6d0659208d
SuperH0ng <ghdtmdgus100@gmail.com>
2022-08-13 23:01:36 +0900

    [feat] PostActivity 서버 연결 데이터 가져오기 성공..!!

--- app/src/main/java/com/example/within_front/board/BoardActivity.kt
+++ app/src/main/java/com/example/within_front/board/BoardActivity.kt
@@ -8,84 +8,76 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.within_front.board.PostAdapter
 import com.example.within_front.R
-//import okhttp3.*
+import okhttp3.*
 import org.json.JSONArray
 import org.json.JSONObject
 import java.io.IOException
 
 class BoardActivity : AppCompatActivity() {
 
-//    private val client = OkHttpClient()
+    private val client = OkHttpClient()
 
     private val postContainer : RecyclerView by lazy{
         findViewById(R.id.posts)
     }
 
-    private var postList = mutableListOf<Post>(Post("포스트 테스트입니다.","SuperH0ng", "ㅂㄷ제ㅐㅑ겹제ㅐ댜겨베ㅐㄷ쟈겨망ㄴ러;ㅣㄴ아ㅓㄻ;ㅣㄴ아렅크춮,ㅡㅊㅌㅋ푸.,ㅋ틏푸;ㅇㄴ미ㅏ럼ㄴ이;ㅏㅓㄻㅇㄴ;ㅣ라ㅓ", 4, 7,1),
-        Post("포스트2","aaa", "포스트 본문 테스트입니다.", 4, 7,1),
-        Post("포스트3","bbb", "post content test", 4, 7,1),
-        Post("포스트 테스트입니다.","SuperH0ng", "ㅂㄷ제ㅐㅑ겹제ㅐ댜겨베ㅐㄷ쟈겨망ㄴ러;ㅣㄴ아ㅓㄻ;ㅣㄴ아렅크춮,ㅡㅊㅌㅋ푸.,ㅋ틏푸;ㅇㄴ미ㅏ럼ㄴ이;ㅏㅓㄻㅇㄴ;ㅣ라ㅓ", 4, 7,1),
-        Post("포스트2","aaa", "포스트 본문 테스트입니다.", 4, 7,1),
-        Post("포스트3","bbb", "post content test", 4, 7,1),
-        Post("포스트 테스트입니다.","SuperH0ng", "ㅂㄷ제ㅐㅑ겹제ㅐ댜겨베ㅐㄷ쟈겨망ㄴ러;ㅣㄴ아ㅓㄻ;ㅣㄴ아렅크춮,ㅡㅊㅌㅋ푸.,ㅋ틏푸;ㅇㄴ미ㅏ럼ㄴ이;ㅏㅓㄻㅇㄴ;ㅣ라ㅓ", 4, 7,1),
-        Post("포스트2","aaa", "포스트 본문 테스트입니다.", 4, 7,1),
-        Post("포스트3","bbb", "post content test", 4, 7,1))
+    private var postList = mutableListOf<Post>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_board)
-        initRecyclerView()
+//        initRecyclerView()
+        // TODO() getPost()
     }
     private fun initRecyclerView(){
         postContainer.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
         postContainer.setHasFixedSize(true)
         postContainer.adapter = PostAdapter(this, postList)
     }
+
+
+    private fun getPost(category : String){
+        val getPostRequest = Request.Builder().addHeader("Content-Type", "application/json").url("http:52.78.137.155:8080/post/boards/$category").build()
+
+        client.newCall(getPostRequest).enqueue(object: Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                Log.d("fail", "게시물 조회 실패")
+                runOnUiThread{
+                    Toast.makeText(
+                        this@BoardActivity,
+                        "게시물 조회에 실패했습니다. 다시 시도해주세요.",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                if(response.code() == 200){
+                    postList = mutableListOf()
+
+                    val jsonArray = JSONArray(response.body()!!.string())
+
+                    for(idx in 0 until jsonArray.length()){
+                        val tempPost = jsonArray[idx] as JSONObject
+
+                        val postTitle = tempPost.getString("title")
+                        val author = tempPost.getString("authorNickname")
+                        val content = tempPost.getString("content")
+                        val commentCount = 1
+                        val likeCount = tempPost.getInt("liked")
+                        val postId = tempPost.getLong("id")
+
+                        val post = Post(postTitle = postTitle, author = author,
+                            content = content, likeCount = likeCount,
+                            commentCount = commentCount, postId = postId)
+                        postList.add(post)
+                    }
+                    Log.d("postList", postList.size.toString())
+                    runOnUiThread{
+                        initRecyclerView()
+                    }
+                }
+            }
+        })
+    }
 }
-
-//package com.example.within_front.board
-//
-//import android.os.Bundle
-//import android.util.Log
-//import android.widget.Toast
-//import androidx.appcompat.app.AppCompatActivity
-//import androidx.recyclerview.widget.LinearLayoutManager
-//import androidx.recyclerview.widget.RecyclerView
-//import com.example.within_front.board.PostAdapter
-//import com.example.within_front.R
-////import okhttp3.*
-//import org.json.JSONArray
-//import org.json.JSONObject
-//import java.io.IOException
-//
-//class BoardActivity : AppCompatActivity() {
-//
-////    private val client = OkHttpClient()
-//    lateinit var postAdapter: PostAdapter
-//    private var postList = mutableListOf<Post>()
-//
-//
-//    private val postContainer : RecyclerView by lazy{
-//        findViewById(R.id.posts)
-//    }
-//
-//
-//    //Post("포스트 테스트입니다.","SuperH0ng", "ㅂㄷ제ㅐㅑ겹제ㅐ댜겨베ㅐㄷ쟈겨망ㄴ러;ㅣㄴ아ㅓㄻ;ㅣㄴ아렅크춮,ㅡㅊㅌㅋ푸.,ㅋ틏푸;ㅇㄴ미ㅏ럼ㄴ이;ㅏㅓㄻㅇㄴ;ㅣ라ㅓ", 4, 7),
-//    //        Post("포스트2","aaa", "포스트 본문 테스트입니다.", 4, 7),
-//    //        Post("포스트3","bbb", "post content test", 4, 7)
-//
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//        setContentView(R.layout.activity_board)
-//        initRecyclerView()
-//    }
-//    private fun initRecyclerView(){
-////        postContainer.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
-////        postContainer.setHasFixedSize(true)
-////        postContainer.adapter = PostAdapter(this, postList)
-//
-//        postAdapter = PostAdapter(this)
-//        rv_post.adapter = postAdapter
-//    }
-//}
-
--- app/src/main/java/com/example/within_front/board/PostActivity.kt
+++ app/src/main/java/com/example/within_front/board/PostActivity.kt
@@ -2,6 +2,8 @@ package com.example.within_front.board
 
 import android.os.Bundle
 import android.util.Log
+import android.widget.ImageButton
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -16,26 +18,44 @@ class PostActivity : AppCompatActivity() {
 
     private val client = OkHttpClient()
 
+    private val postTitle: TextView by lazy {
+        findViewById(R.id.title)
+    }
+    private val author: TextView by lazy {
+        findViewById(R.id.nickname)
+    }
+    private val content: TextView by lazy {
+        findViewById(R.id.content)
+    }
+    private val commentCount: TextView by lazy {
+        findViewById(R.id.comment_count)
+    }
+    private val likeCount: TextView by lazy {
+        findViewById(R.id.like_count)
+    }
+    private val date: TextView by lazy {
+        findViewById(R.id.date)
+    }
+
     private val commentContainer : RecyclerView by lazy{
         findViewById(R.id.comments)
     }
 
-    private var commentList = mutableListOf<Comment>(
-        Comment("SuperH0ng", "안녕하세요", "2022-08-13 | 16:03"),
-        Comment("eunsun", "ㅎ2ㅎ2", "2022-08-13 | 16:05"),
-        Comment("SuperH0ng", "안녕하세요", "2022-08-13 | 16:03"),
-        Comment("eunsun", "ㅎ2ㅎ2", "2022-08-13 | 16:05"),
-        Comment("SuperH0ng", "안녕하세요", "2022-08-13 | 16:03"),
-        Comment("eunsun", "ㅎ2ㅎ2", "2022-08-13 | 16:05"),
-        Comment("SuperH0ng", "안녕하세요", "2022-08-13 | 16:03"),
-        Comment("eunsun", "ㅎ2ㅎ2", "2022-08-13 | 16:05")
-    )
+    private var commentList = mutableListOf<Comment>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_post)
-//        initRecyclerView()
-        getComment(1)
+        val intent = intent
+
+        // defalut 0으로 바꿔주어야 함
+        val postId = intent.getLongExtra("postId", 1)
+        if(postId == 0L){
+            Toast.makeText(this, "게시글 조회에 실패했습니다.", Toast.LENGTH_SHORT).show()
+            finish()
+        }
+        getComments(postId)
+        getPost(postId)
     }
 
     private fun initRecyclerView(){
@@ -44,8 +64,8 @@ class PostActivity : AppCompatActivity() {
         commentContainer.adapter = CommentAdapter(this, commentList)
     }
 
-    private fun getComment(postId : Long){
-        val getCommentRequest = Request.Builder().addHeader("Content-Type", "application/json").url("http://52.78.137.155:8080/post/boards/1/comments").build()
+    private fun getComments(postId : Long){
+        val getCommentRequest = Request.Builder().addHeader("Content-Type", "application/json").url("http://52.78.137.155:8080/post/boards/$postId/comments").build()
 
         client.newCall(getCommentRequest).enqueue(object: Callback {
             override fun onFailure(call: Call, e: IOException) {
@@ -66,11 +86,10 @@ class PostActivity : AppCompatActivity() {
                     val jsonArray = JSONArray(response.body()!!.string())
 
                     for(idx in 0 until jsonArray.length()){
-                        val tempPost = jsonArray[idx] as JSONObject
-                        val author = tempPost.getString("authorName")
-                        val content = tempPost.getString("content")
-                        val createdAt = tempPost.getString("createdAt")
-
+                        val tempComment = jsonArray[idx] as JSONObject
+                        val author = tempComment.getString("authorName")
+                        val content = tempComment.getString("content")
+                        val createdAt = tempComment.getString("createdAt")
                         val comment = Comment(author = author, content = content, date = createdAt)
                         commentList.add(comment)
                     }
@@ -79,6 +98,37 @@ class PostActivity : AppCompatActivity() {
                     runOnUiThread{
                         initRecyclerView()
                     }
+                }
+            }
+        })
+    }
+
+
+    private fun getPost(postId : Long){
+        val getPostRequest = Request.Builder().addHeader("Content-Type", "application/json").url("http://52.78.137.155:8080/post/boards/posts/$postId").build()
+
+        client.newCall(getPostRequest).enqueue(object: Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                Log.d("fail", "게시물 조회 실패")
+                runOnUiThread{
+                    Toast.makeText(
+                        this@PostActivity,
+                        "댓글 조회에 실패했습니다. 다시 시도해주세요.",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                if(response.code() == 200){
+                    commentList = mutableListOf()
+                    val tempPost = JSONObject(response.body()!!.string())
+                    postTitle.text = tempPost.getString("title")
+                    author.text = tempPost.getString("authorNickname")
+                    content.text = tempPost.getString("content")
+                    commentCount.text = 1.toString()
+                    likeCount.text = tempPost.getInt("liked").toString()
+                    date.text = tempPost.getString("createdAt")
                 }
             }
         })
