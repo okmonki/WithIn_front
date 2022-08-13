fee67f23a92227c785f0276f09325f44ac99a07e
SuperH0ng <ghdtmdgus100@gmail.com>
2022-08-13 19:18:05 +0900

    [ERR] postActivity

--- app/src/main/java/com/example/within_front/board/PostActivity.kt
+++ app/src/main/java/com/example/within_front/board/PostActivity.kt
@@ -2,6 +2,7 @@ package com.example.within_front.board
 
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -33,12 +34,53 @@ class PostActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_post)
-        Log.d("test", commentList.size.toString())
-        initRecyclerView()
+//        initRecyclerView()
+        getComment(1)
     }
+
     private fun initRecyclerView(){
         commentContainer.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
         commentContainer.setHasFixedSize(true)
         commentContainer.adapter = CommentAdapter(this, commentList)
+    }
+
+    private fun getComment(postId : Long){
+        val getCommentRequest = Request.Builder().addHeader("Content-Type", "application/json").url("http:localhost:8080/post/boards/$postId/comments").build()
+
+        client.newCall(getCommentRequest).enqueue(object: Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                Log.d("fail", "댓글 조회 실패")
+                runOnUiThread{
+                    Toast.makeText(
+                        this@PostActivity,
+                        "댓글 조회에 실패했습니다. 다시 시도해주세요.",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                if(response.code() == 200){
+                    commentList = mutableListOf()
+
+                    val jsonArray = JSONArray(response.body()!!.string())
+
+                    for(idx in 0 until jsonArray.length()){
+                        val tempPost = jsonArray[idx] as JSONObject
+                        val author = tempPost.getString("authorName")
+                        val content = tempPost.getString("content")
+                        val createdAt = tempPost.getString("createdAt")
+
+                        val comment = Comment(author = author, content = content, date = createdAt)
+                        commentList.add(comment)
+                    }
+
+                    Log.d("commentList", commentList.size.toString())
+                    runOnUiThread{
+                        initRecyclerView()
+                    }
+                }
+            }
+        })
     }
 }
