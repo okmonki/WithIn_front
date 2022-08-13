ef8d0962b0e331ae5c3a206ceb39ae0869566bf5
SuperH0ng <ghdtmdgus100@gmail.com>
2022-08-13 10:27:15 +0900

    [no issue]

--- app/src/main/java/com/example/within_front/board/BoardActivity.kt
+++ app/src/main/java/com/example/within_front/board/BoardActivity.kt
@@ -1,44 +1,3 @@
-//package com.example.within_front.board
-//
-//import android.os.Bundle
-//import android.util.Log
-//import android.widget.Toast
-//import androidx.appcompat.app.AppCompatActivity
-//import androidx.recyclerview.widget.LinearLayoutManager
-//import androidx.recyclerview.widget.RecyclerView
-//import com.example.within_front.board.PostAdapter
-//import com.example.within_front.R
-////import okhttp3.*
-//import org.json.JSONArray
-//import org.json.JSONObject
-//import java.io.IOException
-//
-//class BoardActivity : AppCompatActivity() {
-//
-////    private val client = OkHttpClient()
-//
-//    private val postContainer : RecyclerView by lazy{
-//        findViewById(R.id.posts)
-//    }
-//
-//    private var postList = mutableListOf<Post>()
-//
-//    //Post("포스트 테스트입니다.","SuperH0ng", "ㅂㄷ제ㅐㅑ겹제ㅐ댜겨베ㅐㄷ쟈겨망ㄴ러;ㅣㄴ아ㅓㄻ;ㅣㄴ아렅크춮,ㅡㅊㅌㅋ푸.,ㅋ틏푸;ㅇㄴ미ㅏ럼ㄴ이;ㅏㅓㄻㅇㄴ;ㅣ라ㅓ", 4, 7),
-//    //        Post("포스트2","aaa", "포스트 본문 테스트입니다.", 4, 7),
-//    //        Post("포스트3","bbb", "post content test", 4, 7)
-//
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//        setContentView(R.layout.activity_board)
-//        initRecyclerView()
-//    }
-//    private fun initRecyclerView(){
-//        postContainer.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
-//        postContainer.setHasFixedSize(true)
-//        postContainer.adapter = PostAdapter(this, postList)
-//    }
-//}
-
 package com.example.within_front.board
 
 import android.os.Bundle
@@ -57,14 +16,12 @@ import java.io.IOException
 class BoardActivity : AppCompatActivity() {
 
 //    private val client = OkHttpClient()
-    lateinit var postAdapter: PostAdapter
-    private var postList = mutableListOf<Post>()
-
 
     private val postContainer : RecyclerView by lazy{
         findViewById(R.id.posts)
     }
 
+    private var postList = mutableListOf<Post>()
 
     //Post("포스트 테스트입니다.","SuperH0ng", "ㅂㄷ제ㅐㅑ겹제ㅐ댜겨베ㅐㄷ쟈겨망ㄴ러;ㅣㄴ아ㅓㄻ;ㅣㄴ아렅크춮,ㅡㅊㅌㅋ푸.,ㅋ틏푸;ㅇㄴ미ㅏ럼ㄴ이;ㅏㅓㄻㅇㄴ;ㅣ라ㅓ", 4, 7),
     //        Post("포스트2","aaa", "포스트 본문 테스트입니다.", 4, 7),
@@ -76,12 +33,55 @@ class BoardActivity : AppCompatActivity() {
         initRecyclerView()
     }
     private fun initRecyclerView(){
-//        postContainer.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
-//        postContainer.setHasFixedSize(true)
-//        postContainer.adapter = PostAdapter(this, postList)
-
-        postAdapter = PostAdapter(this)
-        rv_post.adapter = postAdapter
+        postContainer.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+        postContainer.setHasFixedSize(true)
+        postContainer.adapter = PostAdapter(this, postList)
     }
 }
+
+//package com.example.within_front.board
+//
+//import android.os.Bundle
+//import android.util.Log
+//import android.widget.Toast
+//import androidx.appcompat.app.AppCompatActivity
+//import androidx.recyclerview.widget.LinearLayoutManager
+//import androidx.recyclerview.widget.RecyclerView
+//import com.example.within_front.board.PostAdapter
+//import com.example.within_front.R
+////import okhttp3.*
+//import org.json.JSONArray
+//import org.json.JSONObject
+//import java.io.IOException
+//
+//class BoardActivity : AppCompatActivity() {
+//
+////    private val client = OkHttpClient()
+//    lateinit var postAdapter: PostAdapter
+//    private var postList = mutableListOf<Post>()
+//
+//
+//    private val postContainer : RecyclerView by lazy{
+//        findViewById(R.id.posts)
+//    }
+//
+//
+//    //Post("포스트 테스트입니다.","SuperH0ng", "ㅂㄷ제ㅐㅑ겹제ㅐ댜겨베ㅐㄷ쟈겨망ㄴ러;ㅣㄴ아ㅓㄻ;ㅣㄴ아렅크춮,ㅡㅊㅌㅋ푸.,ㅋ틏푸;ㅇㄴ미ㅏ럼ㄴ이;ㅏㅓㄻㅇㄴ;ㅣ라ㅓ", 4, 7),
+//    //        Post("포스트2","aaa", "포스트 본문 테스트입니다.", 4, 7),
+//    //        Post("포스트3","bbb", "post content test", 4, 7)
+//
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_board)
+//        initRecyclerView()
+//    }
+//    private fun initRecyclerView(){
+////        postContainer.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+////        postContainer.setHasFixedSize(true)
+////        postContainer.adapter = PostAdapter(this, postList)
+//
+//        postAdapter = PostAdapter(this)
+//        rv_post.adapter = postAdapter
+//    }
+//}
 
--- app/src/main/java/com/example/within_front/board/PostAdapter.kt
+++ app/src/main/java/com/example/within_front/board/PostAdapter.kt
@@ -1,60 +1,3 @@
-//package com.example.within_front.board
-//
-//import android.content.Context
-//import android.util.Log
-//import android.view.LayoutInflater
-//import android.view.View
-//import android.view.ViewGroup
-//import android.widget.TextView
-//import androidx.recyclerview.widget.RecyclerView
-//import com.example.within_front.R
-//import com.example.within_front.board.Post
-//
-//class PostAdapter(val context: Context, private var postList : MutableList<Post>) : RecyclerView.Adapter<PostAdapter.CustomViewHolder>() {
-//
-//    inner class CustomViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView){
-//        private val postTitle = itemView.findViewById<TextView>(R.id.post_title)
-//        private val content = itemView.findViewById<TextView>(R.id.content)
-//        private val author = itemView.findViewById<TextView>(R.id.author)
-//        private val commentCount = itemView.findViewById<TextView>(R.id.comment_count)
-//        private val likeCount = itemView.findViewById<TextView>(R.id.like_count)
-//
-////        private val boardName = itemView.findViewById<TextView>(R.id.board_name)
-////        private val date = itemView.findViewById<TextView>(R.id.date)
-//
-//        fun bind(post : Post){
-//            postTitle.text = post.postTitle
-//            author.text = post.author
-//            content.text = post.content
-//            commentCount.text = post.commentCount.toString()
-//            likeCount.text = post.likeCount.toString()
-//
-//
-////            boardName.text = post.boardName
-////            date.text = post.date
-//        }
-//    }
-//
-//    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CustomViewHolder {
-//        val view = LayoutInflater.from(parent.context).inflate(R.layout.post_item, parent, false)
-//        return CustomViewHolder(view)
-//    }
-//
-//    override fun onBindViewHolder(holder: CustomViewHolder, position: Int) {
-//        val post = postList[position]
-//        holder.apply{
-//            bind(post)
-//        }
-//    }
-//
-//    override fun getItemCount(): Int {
-//        Log.d("size", postList.size.toString())
-//        return postList.size
-//    }
-//}
-
-
-
 package com.example.within_front.board
 
 import android.content.Context
@@ -67,9 +10,7 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.within_front.R
 import com.example.within_front.board.Post
 
-class PostAdapter(private val context: Context) : RecyclerView.Adapter<PostAdapter.CustomViewHolder>() {
-
-    var postList = mutableListOf<Post>()
+class PostAdapter(val context: Context, private var postList : MutableList<Post>) : RecyclerView.Adapter<PostAdapter.CustomViewHolder>() {
 
     inner class CustomViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView){
         private val postTitle = itemView.findViewById<TextView>(R.id.post_title)
@@ -100,11 +41,10 @@ class PostAdapter(private val context: Context) : RecyclerView.Adapter<PostAdapt
     }
 
     override fun onBindViewHolder(holder: CustomViewHolder, position: Int) {
-//        val post = postList[position]
-//        holder.apply{
-//            bind(post)
-//        }
-        holder.bind(postList[position])
+        val post = postList[position]
+        holder.apply{
+            bind(post)
+        }
     }
 
     override fun getItemCount(): Int {
@@ -112,3 +52,63 @@ class PostAdapter(private val context: Context) : RecyclerView.Adapter<PostAdapt
         return postList.size
     }
 }
+
+
+
+//package com.example.within_front.board
+//
+//import android.content.Context
+//import android.util.Log
+//import android.view.LayoutInflater
+//import android.view.View
+//import android.view.ViewGroup
+//import android.widget.TextView
+//import androidx.recyclerview.widget.RecyclerView
+//import com.example.within_front.R
+//import com.example.within_front.board.Post
+//
+//class PostAdapter(private val context: Context) : RecyclerView.Adapter<PostAdapter.CustomViewHolder>() {
+//
+//    var postList = mutableListOf<Post>()
+//
+//    inner class CustomViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView){
+//        private val postTitle = itemView.findViewById<TextView>(R.id.post_title)
+//        private val content = itemView.findViewById<TextView>(R.id.content)
+//        private val author = itemView.findViewById<TextView>(R.id.author)
+//        private val commentCount = itemView.findViewById<TextView>(R.id.comment_count)
+//        private val likeCount = itemView.findViewById<TextView>(R.id.like_count)
+//
+////        private val boardName = itemView.findViewById<TextView>(R.id.board_name)
+////        private val date = itemView.findViewById<TextView>(R.id.date)
+//
+//        fun bind(post : Post){
+//            postTitle.text = post.postTitle
+//            author.text = post.author
+//            content.text = post.content
+//            commentCount.text = post.commentCount.toString()
+//            likeCount.text = post.likeCount.toString()
+//
+//
+////            boardName.text = post.boardName
+////            date.text = post.date
+//        }
+//    }
+//
+//    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CustomViewHolder {
+//        val view = LayoutInflater.from(parent.context).inflate(R.layout.post_item, parent, false)
+//        return CustomViewHolder(view)
+//    }
+//
+//    override fun onBindViewHolder(holder: CustomViewHolder, position: Int) {
+////        val post = postList[position]
+////        holder.apply{
+////            bind(post)
+////        }
+//        holder.bind(postList[position])
+//    }
+//
+//    override fun getItemCount(): Int {
+//        Log.d("size", postList.size.toString())
+//        return postList.size
+//    }
+//}
